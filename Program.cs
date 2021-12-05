af84a4ac2265deb54e05641ad8f57550cb3c2acd
Ilia Detchenkov <detchenkov@bk.ru>
2021-12-05 15:54:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab01_immutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
